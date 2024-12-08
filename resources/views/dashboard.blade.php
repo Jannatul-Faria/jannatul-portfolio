ba037d5b842ccfc4dd2981cb7121886a19412107
Jannatul-faria <kairasana8@gmail.com>
2024-12-08 23:16:45 +0600

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,7 +17,9 @@
                     <form id="send-verification" method="post" action="{{ route('verification.send') }}">
                         @csrf
                     </form>
-
+                    <div class="flex justify-between ">
+                        
+                    </div>
                     <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
                         @csrf
                         @method('patch')
@@ -66,6 +68,47 @@
                             @endif
                         </div>
                     </form>
+
+
+                    <form method="" action="" class="mt-6 space-y-6">
+                        <div>
+                            <x-input-label for="position_title" :value="__('position_title')" />
+                            <x-text-input id="position_title" name="position_title" type="text" class="mt-1 block w-full" :value="old('position_title', $user->position_title)" required autofocus autocomplete="position_title" />
+                            <x-input-error class="mt-2" :messages="$errors->get('position_title')" />
+                        </div>
+                        <div>
+                            <x-input-label for="About_section_heading" :value="__('About_section_heading')" />
+                            <x-text-input id="About_section_heading" name="About_section_heading" type="text" class="mt-1 block w-full" :value="old('About_section_heading', $user->About_section_heading)" required autofocus autocomplete="About_section_heading" />
+                            <x-input-error class="mt-2" :messages="$errors->get('About_section_heading')" />
+                        </div>
+                        <div>
+                            <x-input-label for="About_section_description" :value="__('About section description')" />
+                            <x-text-input id="About_section_description" name="About_section_description" type="text" class="mt-1 block w-full" :value="old('About_section_description', $user->About_section_description)" required autofocus autocomplete="About_section_description" />
+                            <x-input-error class="mt-2" :messages="$errors->get('About_section_description')" />
+                        </div>
+                        <div>
+                            <x-input-label for="About_section_row" :value="__('About_section_row')" />
+                            <x-text-input id="About_section_row" name="About_section_row" type="text" class="mt-1 block w-full" :value="old('About_section_row', $user->About_section_row)" required autofocus autocomplete="About_section_row" />
+                            <x-input-error class="mt-2" :messages="$errors->get('About_section_row')" />
+                        </div>
+                        <div>
+                            <x-input-label for="education_title" :value="__('education_title')" />
+                            <x-text-input id="education_title" name="education_title" type="text" class="mt-1 block w-full" :value="old('education_title', $user->education_title)" required autofocus autocomplete="education_title" />
+                            <x-input-error class="mt-2" :messages="$errors->get('education_title')" />
+                        </div>
+                        <div>
+                            <x-input-label for="education_institude" :value="__('education_institude')" />
+                            <x-text-input id="education_institude" name="education_institude" type="text" class="mt-1 block w-full" :value="old('education_institude', $user->education_institude)" required autofocus autocomplete="education_institude" />
+                            <x-input-error class="mt-2" :messages="$errors->get('education_institude')" />
+                        </div>
+                        <div>
+                            <x-input-label for="education_year_start_date" :value="__('education_year_start_date')" />
+                            <x-text-input id="education_year_start_date" name="education_year_start_date" type="date" class="mt-1 block w-full" :value="old('education_year_start_date', $user->education_year_start_date)" required autofocus autocomplete="education_year_start_date" />
+                            <x-input-error class="mt-2" :messages="$errors->get('education_year_start_date')" />
+                        </div>
+
+                    </form>
+                   
                     </section>
                 </div>
             </div>
